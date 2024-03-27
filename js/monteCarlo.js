6d8a28b60987acc79b4834610117834b41de6f0c
p0daru <102858022+p0daru@users.noreply.github.com>
2024-03-27 23:29:20 +0200

    Update monteCarlo.js

--- js/monteCarlo.js
+++ js/monteCarlo.js
@@ -15,27 +15,6 @@ function calcSquareArea(squaredRadius) {
   return squareArea.toFixed(2);
 }
 
-// function calcCircleArea(n, a, b, c) {
-//   const random = Math.random;
-//   const squareArea = calcSquareArea(c);
-//   let m = 0; // Лічильник точок всередині кола
-
-//   for (let i = 0; i < n; i++) {
-//     const x = random() * (15.62 - 0.39) + 0.39; // випадкове число x в діапазоні (0.39, 15.62)
-//     const y = random() * (13.57 - -1.66) + -1.66; // випадкове число y в діапазоні (-1.66, 13.57)
-
-//     // Обчислення квадрату відстані від точки до центру кола
-//     const distanceSquared = Math.pow(x - a, 2) + Math.pow(y - b, 2);
-
-//     // Якщо точка знаходиться всередині кола, збільшуємо лічильник m
-//     if (distanceSquared <= c) {
-//       m++;
-//     }
-//   }
-
-//   return (m / n) * squareArea;
-// }
-
 function calcPoints(n, a, b, c) {
   const random = Math.random;
   let points = 0; // Лічильник точок всередині кола
